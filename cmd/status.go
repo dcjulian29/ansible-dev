b5b7d76ff6dc0541c1235306ba059e9dde6aab75
Julian Easterling <julian@julianscorner.com>
2023-04-19 00:49:09 -0400

    Added a command to show the status of the vagrant environment

---
+++ cmd/status.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2023 Julian Easterling <julian@julianscorner.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	statusCmd = &cobra.Command{
+		Use:   "status",
+		Short: "Output status of the Ansible development vagrant environment",
+		Long:  "Output status of the Ansible development vagrant environment",
+		Run: func(cmd *cobra.Command, args []string) {
+			vagrant_status()
+			ensureWorkingDirectoryAndExit()
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
+
+func vagrant_status() {
+	ensureAnsibleDirectory()
+	ensureVagrantfile()
+
+	executeExternalProgram("vagrant", "status")
+}
