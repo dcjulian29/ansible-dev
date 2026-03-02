d74d034f4395f4eb8a14ae41082a20f2d77d1e55
Julian Easterling <julian@julianscorner.com>
2026-03-01 19:56:06 -0500

    Refactored Upgrade command to separate package

--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -13,35 +13,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package cmd
+package upgrade
 
 import (
+	"errors"
+
+	"github.com/dcjulian29/ansible-dev/internal/ansible"
+	"github.com/dcjulian29/ansible-dev/internal/vagrant"
 	"github.com/spf13/cobra"
 )
 
-var (
-	upgradeCmd = &cobra.Command{
+func NewCommand() *cobra.Command {
+	cmd := &cobra.Command{
 		Use:   "upgrade",
 		Short: "Updates the boxes that the Ansible development vagrant environment uses",
-		Long:  "Updates the boxes that the Ansible development vagrant environment uses",
-		Run: func(cmd *cobra.Command, args []string) {
-			vagrant_upgrade()
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return vagrant.Upgrade()
 		},
-		PreRun: func(cmd *cobra.Command, args []string) {
-			ensureAnsibleDirectory()
-			ensureVagrantfile()
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := ansible.EnsureAnsibleDirectory(); err != nil {
+				return errors.New("not an Ansible development directory")
+			}
+
+			return vagrant.EnsureVagrantfile()
 		},
 	}
-)
 
-func init() {
-	rootCmd.AddCommand(upgradeCmd)
-}
-
-func vagrant_upgrade() {
-	update := []string{"box", "update"}
-	prune := []string{"box", "prune", "--force", "--keep-active-boxes"}
-
-	executeExternalProgram("vagrant", update...)
-	executeExternalProgram("vagrant", prune...)
+	return cmd
 }
