bcd1ff839ffeaaeb8ab67adc9a0b1579d9ce3f7b
Julian Easterling <julian@julianscorner.com>
2026-03-01 19:53:51 -0500

    Refactored Restore command to separate package

---
+++ cmd/restore/restore.go
@@ -0,0 +1,65 @@
+/*
+Copyright © 2026 Julian Easterling <julian@julianscorner.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package restore
+
+import (
+	"errors"
+
+	"github.com/dcjulian29/ansible-dev/internal/ansible"
+	"github.com/dcjulian29/go-toolbox/execute"
+	"github.com/spf13/cobra"
+)
+
+func NewCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "restore",
+		Short: "Restore Ansible collections and roles from the requirements.yml file",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			verbose, _ := cmd.Flags().GetBool("verbose")
+			force, _ := cmd.Flags().GetBool("force")
+
+			param := []string{"install"}
+
+			if verbose {
+				param = append(param, "-v")
+			}
+
+			if force {
+				param = append(param, "--force")
+			}
+
+			param = append(param, "-r", "requirements.yml")
+
+			if err := execute.ExternalProgram("ansible-galaxy", param...); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := ansible.EnsureAnsibleDirectory(); err != nil {
+				return errors.New("not an Ansible development directory")
+			}
+
+			return ansible.EnsureRequirementsFile()
+		},
+	}
+
+	cmd.Flags().Bool("force", false, "force overwriting existing roles or collections")
+	cmd.Flags().BoolP("verbose", "v", false, "tell Ansible to print more debug messages")
+
+	return cmd
+}
