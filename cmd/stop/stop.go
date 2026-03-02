8be2c907a22bd922aa36404bed58384497449932
Julian Easterling <julian@julianscorner.com>
2026-03-01 19:56:45 -0500

    Refactored Stop command to separate package

--- cmd/stop/stop.go
+++ cmd/stop/stop.go
@@ -13,49 +13,32 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package cmd
+package stop
 
 import (
-	"fmt"
-	"strings"
+	"errors"
 
+	"github.com/dcjulian29/ansible-dev/internal/ansible"
+	"github.com/dcjulian29/ansible-dev/internal/vagrant"
 	"github.com/spf13/cobra"
-	"gopkg.in/ini.v1"
 )
 
-var (
-	stopCmd = &cobra.Command{
+func NewCommand() *cobra.Command {
+	cmd := &cobra.Command{
 		Use:     "stop",
 		Aliases: []string{"down"},
 		Short:   "Stops the Ansible development vagrant environment",
-		Long:    "Stops the Ansible development vagrant environment",
-		Run: func(cmd *cobra.Command, args []string) {
-			vagrant_down()
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return vagrant.Down()
 		},
-		PreRun: func(cmd *cobra.Command, args []string) {
-			ensureAnsibleDirectory()
-			ensureVagrantfile()
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := ansible.EnsureAnsibleDirectory(); err != nil {
+				return errors.New("not an Ansible development directory")
+			}
+
+			return vagrant.EnsureVagrantfile()
 		},
 	}
-)
 
-func init() {
-	rootCmd.AddCommand(stopCmd)
-}
-
-func vagrant_down() {
-	inv, err := ini.Load("hosts.ini")
-	cobra.CheckErr(err)
-
-	section, err := inv.GetSection("vagrant")
-	cobra.CheckErr(err)
-
-	for _, vm := range section.KeyStrings() {
-		name := strings.Split(vm, " ")[0]
-
-		fmt.Printf(Yellow("\nStopping '%s'...\n\n"), name)
-
-		executeExternalProgram("vagrant", "halt", name)
-	}
-
+	return cmd
 }
